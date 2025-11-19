1b5d0288fe71de2deed1200d0c85787ff0bca7b4
temitopeohassan <temitope.hassan.2@gmail.com>
2025-11-19 17:47:59 +0100

    Code reverted to Victor's final commit

--- internal/models/invoice.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type CreateInvoiceRequest struct{}
-type Invoice struct{}
-type InvoiceListResponse struct{}
-type UpdateInvoiceStatusRequest struct{}
-type InvoiceGenerationJob struct{}
